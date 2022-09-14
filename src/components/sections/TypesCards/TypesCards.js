e666496e82d26ff9cf5c5c15fa310e5b27090879
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-09-14 16:57:44 +0300

    fix: fixed layout for 2 types morph

--- src/components/sections/TypesCards/TypesCards.js
+++ src/components/sections/TypesCards/TypesCards.js
@@ -1,50 +1,58 @@
-import Dot from "../../common/Dot/Dot"
-import Paragraph from "../../common/Paragraph/Paragraph"
+import Paragraph from "../../common/Paragraph/Paragraph";
 
-import classes from './styles.module.scss';
+import classes from "./styles.module.scss";
 
-import One from '../../../assets/images/one.svg'
-import Two from '../../../assets/images/two.svg'
+import first from "../../../assets/images/first.svg";
+import second from "../../../assets/images/second.svg";
 
-import MihoMorph from '../../../assets/images/miho-morph.png'
-import MihoID from '../../../assets/images/miho-id.png'
-import HenriMorph from '../../../assets/images/henri-morph.png'
-import HenriID from '../../../assets/images/henri-id.png'
+import MihoMorph from "../../../assets/images/miho-morph.png";
+import MihoID from "../../../assets/images/miho-id.png";
+import HenriMorph from "../../../assets/images/henri-morph.png";
+import HenriID from "../../../assets/images/henri-id.png";
 
 export default function TypesCards() {
   return (
     <div className={classes.containerTypesCards}>
       <div className={classes.morphBlock}>
-        <img className={classes.mihoMainImage} src={MihoMorph} alt=""/>
+        <img className={classes.mihoMainImage} src={MihoMorph} alt="miho" />
 
-        <div className={classes.classiqueContent}>
+        <div className={classes.contentBlock}>
           <p className={classes.classiqueParagraph}>
-            <img className={classes.classiqueBorder} src={One} alt=""/>
+            <img className={classes.classiqueBorder} src={first} alt="classique" />
             <h4 className={classes.title}>CLASSIQUE</h4>
           </p>
-          <Paragraph className={classes.mihoText}>
-            Metamorph è un avventura dinamica giocabile in terza persona di genere life simulator
-            ambientato nel mondo virtuale Fata Morgana<Dot/>
+          <Paragraph className={`${classes.descr} ${classes.mihoText}`}>
+            This is the classic mode of the game where each player can start
+            their experience immediately to learn the basic mechanics and then
+            decide whether to switch to Eternal mode
           </Paragraph>
-          <img className={classes.idImage} src={MihoID} alt=""/>
+          <img className={classes.idImage} src={MihoID} alt="miho id" />
         </div>
       </div>
 
       <div className={`${classes.morphBlock} ${classes.eternalBlock}`}>
-        <div className={classes.eternalContent}>
+        <div className={classes.contentBlock}>
+          
           <p className={classes.eternalParagraph}>
-            <img className={classes.eternalBorder} src={Two} alt=""/>
-            <h4 className={`${classes.title} ${classes.eternalTitle}`}>ETERNAL</h4>
+            <img className={classes.eternalBorder} src={second} alt="eternal border" />
+            <h4 className={`${classes.title} ${classes.eternalTitle}`}>
+              ETERNAL
+            </h4>
           </p>
-          <Paragraph className={classes.henriText}>
-            Metamorph è un avventura dinamica giocabile in terza persona di genere life simulator
-            ambientato nel mondo virtuale Fata Morgana<Dot/>
+          <Paragraph className={`${classes.descr} ${classes.henriText}`}>
+            The Morph Eternal card allows for a full gaming experience where a
+            player can earn in-game currency a Poligon-based cryptocurrency
+            called Morganis
           </Paragraph>
-          <img className={classes.idImage} src={HenriID} alt=""/>
+          <img
+            className={`${classes.idImage} ${classes.idImageHenri}`}
+            src={HenriID}
+            alt="hneri id card"
+          />
         </div>
-        <img src={HenriMorph} alt=""/>
-      </div> 
+
+        <img src={HenriMorph} alt="henri morph" />
+      </div>
     </div>
-    
-  )
+  );
 }
