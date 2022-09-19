988acf40b4ed455391c33689a92c5703eb006bcd
Anastasia Karachova <nastiakarachova@gmail.com>
2022-09-19 17:20:55 +0300

    add mobile layout for morph id

--- src/components/sections/MorphID/MorphID.jsx
+++ src/components/sections/MorphID/MorphID.jsx
@@ -7,20 +7,24 @@ import Paragraph from "../../common/Paragraph/Paragraph";
 import TransparentGreenCard from "../../../assets/images/transparent-green-card.png";
 
 import classes from "./style.module.scss";
+import {useWindowWidth} from "../../../hooks/useWindowWidth";
 
 export const MorphId = () => {
+  const isDesktop = useWindowWidth(1024)
+
   return (
     <div className={classes.container}>
       <div className={classes.cardsImg}>
-        <img src={TransparentGreenCard} alt="cards" />
+        {isDesktop && <img src={TransparentGreenCard} alt="cards" />}
       </div>
 
       <div className={classes.description}>
-        <div className={classes.chineseText}>變形 相同的代碼</div>
+        {isDesktop && <div className={classes.chineseText}>變形 相同的代碼</div>}
 
         <div className={classes.titleWrapper}>
           <Title classname={classes.title}>
-            Morph <GreenText>id</GreenText>
+            Morph {!isDesktop && <br/>} <GreenText>id</GreenText>
+            {!isDesktop && <span className={classes.chineseText}>變形 相同的代碼</span>}
           </Title>
         </div>
 
@@ -30,8 +34,9 @@ export const MorphId = () => {
           Morph that stands out in each user.
         </Paragraph>
 
-        <p className={classes.morhTitle}>2 ways to start</p>
-        <Paragraph className={classes.morhDescr}>
+          <p className={classes.waysToStartChinese}>2 種 方式開始</p>
+          <p className={classes.morhTitle}>2 ways to start</p>
+        <Paragraph className={`${classes.morhDescr} ${classes.waysToStartParagraph}`}>
           The game offers 2 experience scenarios based on the starting Morph ID
           card a player chooses. Each scenario satisfies different game modes.
           Classique card is more suitable for explorers and the Eternal one for
