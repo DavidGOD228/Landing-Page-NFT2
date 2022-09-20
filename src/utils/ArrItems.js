a3a8af3a3eef3fd17febf2b1d290d16fe0e5c95e
Anastasia Karachova <nastiakarachova@gmail.com>
2022-09-20 13:02:06 +0300

    change amount of items at marketplace

--- src/utils/ArrItems.js
+++ src/utils/ArrItems.js
@@ -1,23 +1,34 @@
 const arrImg = [
-	'bad-1.png',
-	'boot-1.png',
-	'boot-2.png',
-	'chair-1.png',
-	'chair-2.png',
-	'pants-1.png',
-	'pants-2.png',
-	'sweater-1.png'
+  'bad-1.png',
+  'boot-1.png',
+  'boot-2.png',
+  'chair-1.png',
+  'chair-2.png',
+  'pants-1.png',
+  'pants-2.png',
+  'sweater-1.png'
 ];
 const getImg = () => Math.round(Math.random() * (arrImg.length - 1));
 
 export const getRandomImg = () => arrImg[getImg()];
 
 export const arrItems = () => {
-	const arr = [];
-	let idx = 0;
-	while (idx < 50) {
-		arr.push(getRandomImg());
-		++idx;
-	}
-	return arr;
+  const width = window.screen.width;
+  let itemsLength = 50
+  if (width >= 550 && width <= 1200) {
+    itemsLength = 40
+  } else if (width > 400 && width <= 550) {
+    itemsLength = 30
+  } else if (width <= 400) {
+    itemsLength = 18
+  }
+
+  console.log(itemsLength)
+  const arr = [];
+  let idx = 0;
+  while (idx < itemsLength) {
+    arr.push(getRandomImg());
+    ++idx;
+  }
+  return arr;
 };
