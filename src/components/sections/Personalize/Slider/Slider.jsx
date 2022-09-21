5d86f57b42689c429eaa40d45de8bc26cd48f622
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-09-21 17:41:09 +0300

    slider animation

--- src/components/sections/Personalize/Slider/Slider.jsx
+++ src/components/sections/Personalize/Slider/Slider.jsx
@@ -1,24 +1,85 @@
-import Phone from '../../../common/Phone/Phone'
-
-import classes from './style.module.scss'
+import { Phone } from 'components';
+import classes from './style.module.scss';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const videos = [
-  "name video from asset",
-  "name video from asset",
-  "name video from asset",
-  "name video from asset",
+  "name video from asse1",
+  "name video from asset2",
+  "name video from asset3",
+  "name video from asset4",
 ]
 
 const Slider = ({ activeSlide, className }) => {
 
+  const renderSLide = (idx) => {
+    return <motion.div
+      initial={{
+        opacity: 1, 
+        scale: 1, 
+        marginRight: 20
+      }}
+      animate={() => {
+        if(idx < activeSlide) return {
+          opacity: 0, 
+          scale: 0.85,
+          width: 0,
+          marginRight: 0
+        }
+
+        if(idx === activeSlide) return {
+          opacity: 1, 
+          scale: 1,
+          transitionEnd: {
+            display: 'hidden'
+          }
+        }
+      }}
+      transition={{
+        duration: 1,
+        ease: 'easeInOut'
+      }}
+    >
+      <Phone classname={classes.sliderItem} />
+    </motion.div>;
+  }
+
   return (
     <div className={`${classes.slider} ${className}`}>
       <div className={classes.shadow} />
-      {videos.map((item, idx) => {
-        if(idx + 1 < activeSlide) return <></>;
+        <AnimatePresence>
+          {renderSLide(1)}
+          {renderSLide(2)}
+          {renderSLide(3)}
+          {renderSLide(4)}
+        </AnimatePresence>
+        {/* {videos.map((item, idx) => {
+          //prev slide
+          if(idx - 1 === activeSlide - 1) return <motion.div
+          initial={{ opacity: 1, scale: 1 }}
+          animate={{ opacity: 0, scale: 0.85 }}
+            transition={{
+              duration: 1,
+		          ease: 'easeInOut'
+            }}
+          >
+            <Phone key={item} classname={classes.sliderItem} />
+          </motion.div>;
 
-        return <Phone key={idx} classname={classes.sliderItem} />
-      })}
+          //prev slide
+          // if(idx + 1 < activeSlide) return <></>;
+         
+
+          return <motion.div
+          initial={{ opacity: 1, scale: 1 }}
+          animate={{ opacity: 0, scale: 0.85 }}
+            transition={{
+              duration: 1,
+		          ease: 'easeInOut'
+            }}
+          >
+            <Phone key={item} classname={classes.sliderItem} />
+          </motion.div>;
+        })} */}
     </div>
   )
 }
