5a810cf2294bd561e552e7d3fa846a2b37a3f8ae
Anastasia Karachova <nastiakarachova@gmail.com>
2022-09-23 20:43:48 +0300

    add morph video

--- src/components/sections/FirstLiveSimulator/FirstLiveSimulator.js
+++ src/components/sections/FirstLiveSimulator/FirstLiveSimulator.js
@@ -2,19 +2,20 @@ import {motion} from 'framer-motion';
 
 import {GlobalContainer, GreenText, Title, Paragraph} from 'components';
 
-import morh from 'assets/images/morh-1.png';
-
 import WhiteLine from 'assets/images/whiteLine.svg';
 import toiletVideo from 'assets/video/toilet.video.mp4';
 import Confidence from 'assets/images/confidence.png';
 import SmileDesktop from 'assets/images/smileDesktop.svg';
+import MorphDesktop from 'assets/video/morphDesktop.video.mp4'
+import MorphMobile from 'assets/video/morphMobile.video.mp4'
 
 import classes from './style.module.scss';
 
-import { paragraphAnimation } from 'farmerMotionsAnimations/paragraphAnimation';
+import {paragraphAnimation} from 'farmerMotionsAnimations/paragraphAnimation';
+import {useWindowWidth} from "../../../hooks/useWindowWidth";
 
 export function FirstLiveSimulator() {
-
+  const isDesktop = useWindowWidth(1024)
   const textPar = `Metamorph is an open-world exploration, NFT creature collector
   and life simulator game built on the Polygon Blockchain,
   releasing on IOS and Android in 2023. Play-to-earn in a
@@ -22,18 +23,12 @@ export function FirstLiveSimulator() {
   you to feel the life inside the game.`
 
   return (
-    <GlobalContainer>
-      <div className={classes.flsDesktopContainer}>
-        <div className={classes.textSection}>
-          <p className={classes.backgroundText}>
-            IMMERSE YOURSELF IN A BREATHING WORLD AND{' '}
-            <GreenText>BECOME A LEGEND</GreenText>
-          </p>
-          <div className={classes.triangle}/>
-          <img className={classes.morh} src={morh} alt='morh'/>
-        </div>
-
-        <div className={classes.content}>
+    <div className={classes.wrapper}>
+      <video className={classes.morphVideo} src={isDesktop ? MorphDesktop : MorphMobile} autoPlay
+             loop muted/>
+      <GlobalContainer>
+        <div className={classes.flsDesktopContainer}>
+          <div className={classes.content}>
             <motion.div
               key="firstLiveTitle"
               className={classes.descr}
@@ -59,56 +54,50 @@ export function FirstLiveSimulator() {
               <Paragraph className={classes.paragraph}>
                 {paragraphAnimation(textPar)}
               </Paragraph>
-
-            <div className={classes.respect}>
-              <p className={classes.privacyText}>Respect his Privacy!</p>
-              <img className={classes.smile} src={SmileDesktop} alt=''/>
-            </div>
+              <div className={classes.respect}>
+                <p className={classes.privacyText}>Respect his Privacy!</p>
+                <img className={classes.smile} src={SmileDesktop} alt=''/>
+              </div>
             </motion.div>
-
-
-          <div className={classes.mobileContainer}>
-
-
-            <div className={classes.imagesBlock}>
-              <motion.div
-                key="firstLiveFirstImage"
-                className={classes.mobileContainer}
-                initial={{y: 150}}
-                whileInView={{y: 0}}
-                transition={{
-                  duration: 1.5,
-                  ease: 'easeInOut'
-                }}
-                viewport={{once: true}}
-              >
-              <video
-                className={classes.privacyImage}
-                src={toiletVideo}
-                autoPlay
-                loop
-                muted
-              />
-            </motion.div>
-            <motion.div
-                key="firstLiveSecondImage"
-                className={classes.secondImage}
-                initial={{y: 150}}
-                whileInView={{y: 0}}
-                transition={{
-                  duration: 1,
-                  ease: 'easeInOut'
-                }}
-                viewport={{once: true}}
-              >
-                <img className={classes.privacyImage} src={Confidence} alt=''/>
-              </motion.div>
+            <div className={classes.mobileContainer}>
+              <div className={classes.imagesBlock}>
+                <motion.div
+                  key="firstLiveFirstImage"
+                  className={classes.mobileContainer}
+                  initial={{y: 150}}
+                  whileInView={{y: 0}}
+                  transition={{
+                    duration: 1.5,
+                    ease: 'easeInOut'
+                  }}
+                  viewport={{once: true}}
+                >
+                  <video
+                    className={classes.privacyImage}
+                    src={toiletVideo}
+                    autoPlay
+                    loop
+                    muted
+                  />
+                </motion.div>
+                <motion.div
+                  key="firstLiveSecondImage"
+                  className={classes.secondImage}
+                  initial={{y: 150}}
+                  whileInView={{y: 0}}
+                  transition={{
+                    duration: 1,
+                    ease: 'easeInOut'
+                  }}
+                  viewport={{once: true}}
+                >
+                  <img className={classes.privacyImage} src={Confidence} alt=''/>
+                </motion.div>
+              </div>
             </div>
-
           </div>
-
         </div>
-      </div>
-    </GlobalContainer>
+      </GlobalContainer>
+    </div>
   );
 }
