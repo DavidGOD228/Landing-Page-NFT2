f90435cd1846650f2f38e34fb0721f80972a1753
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-09-23 13:55:32 +0300

    anim text

--- src/components/common/GreenText/GreenText.jsx
+++ src/components/common/GreenText/GreenText.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 import classes from './style.module.scss'
 
-export const GreenText = ({children, classname, style}) => {
+export const GreenText = React.forwardRef(({children, classname, style}, ref) => {
   return (
-    <span style={style} className={`${classes.greenText} ${classname}`}>{children}</span>
+    <span
+      ref={ref}
+      style={style}
+      className={`${classes.greenText} ${classname}`}
+    >
+      {children}
+    </span>
   )
-}
+})
 
 export default GreenText
