88e22a55eda983668ca3528f7e40e2b4cf6b5ddd
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-09-26 18:28:18 +0300

    anim

--- src/components/common/Title/Title.jsx
+++ src/components/common/Title/Title.jsx
@@ -1,35 +1,45 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 import classes from './style.module.scss';
 
-export const Title = ({isAfterBorder = false, children, style, classname, classnameWrapper}) => {
-  const isAfterBrd = isAfterBorder ? classes.isAfter : classes.isBefore;
-  
-  return (
-    <div className={`${classes.content} ${classnameWrapper}`}>
-      <motion.div 
-        className={`${classes.line} ${isAfterBrd}`}
-        initial={{
-          height: 0,
-          width: 0,
-        }}
-        whileInView={{
-          height: '95%',
-          width: 3,
-        }}
-        transition={{
-          duration: 1,
-          delay: 0.25,
-          origin: 1,
-          ease: 'easeOut'
-        }}
-        viewport={{ once: true }}
-      />
-      <div className={`${classes.shadowUnderLine} ${isAfterBrd}`} />
-      <h2 style={style} className={`${classes.title} ${classname}`}>
-        {children}
-      </h2>
-    </div>
-  )
-}
+export const Title = ({
+	isAfterBorder = false,
+	children,
+	style,
+	classname,
+	classnameWrapper,
+  animWrapperContainer
+}) => {
+	const isAfterBrd = isAfterBorder ? classes.isAfter : classes.isBefore;
 
-export default Title
+	return (
+		<div className={`${classes.content} ${classnameWrapper}`}>
+			<motion.div
+				className={`${classes.line} ${isAfterBrd}`}
+				initial={{
+					height: 0,
+					width: 0,
+          ...animWrapperContainer?.initial
+				}}
+				whileInView={{
+					height: '95%',
+					width: 3,
+          ...animWrapperContainer?.whileInView
+				}}
+				transition={{
+					duration: 1,
+					delay: 0.25,
+					origin: 1,
+					ease: 'easeOut',
+          ...animWrapperContainer?.transition
+				}}
+				viewport={{ once: true }}
+			/>
+			<div className={`${classes.shadowUnderLine} ${isAfterBrd}`} />
+			<h2 style={style} className={`${classes.title} ${classname}`}>
+				{children}
+			</h2>
+		</div>
+	);
+};
+
+export default Title;
--- src/components/sections/TypesCards/TypesCards.js
+++ src/components/sections/TypesCards/TypesCards.js
@@ -1,70 +1,82 @@
-import { GlobalContainer, Paragraph } from "../../";
+import { GlobalContainer, Paragraph } from 'components';
 
-import classes from "./styles.module.scss";
+import classes from './styles.module.scss';
 
-import first from "../../../assets/images/first.svg";
-import second from "../../../assets/images/second.svg";
+import first from 'assets/images/first.svg';
+import second from 'assets/images/second.svg';
 
-import MihoMorph from "../../../assets/images/morphs/miho-ikuta/miho-morph-presentation.png";
-import MihoMorphMobile from "../../../assets/images/miho-morph.png";
-import MihoID from "../../../assets/images/miho-id.png";
-import HenriMorph from "../../../assets/images/morphs/henrik-hail/henrik-hail-presentation.png";
-import HenriMorphDesktop from "../../../assets/images/henri-morph.png";
-import HenriID from "../../../assets/images/henri-id.png";
-import {useWindowWidth} from "../../../hooks/useWindowWidth";
-import {morphIDText} from "../../../data/morphID/text";
+import MihoMorph from 'assets/images/morphs/miho-ikuta/miho-morph-presentation.png';
+import MihoMorphMobile from 'assets/images/miho-morph.png';
+import MihoID from 'assets/images/miho-id.png';
+import HenriMorph from 'assets/images/morphs/henrik-hail/henrik-hail-presentation.png';
+import HenriMorphDesktop from 'assets/images/henri-morph.png';
+import HenriID from 'assets/images/henri-id.png';
+import { useWindowWidth } from 'hooks/useWindowWidth';
+import { morphIDText } from 'data/morphID/text';
 
 export function TypesCards() {
-  const isDesktop = useWindowWidth(1024)
-  return (
-    <div className={classes.wrapper}>
-    <GlobalContainer>
-      <div className={classes.containerTypesCards}>
-        <div className={classes.morphBlock}>
-          <img className={classes.mihoMainImage} src={isDesktop ? MihoMorph: MihoMorphMobile} alt="miho" />
+	const isDesktop = useWindowWidth(1024);
+	return (
+		<div className={classes.wrapper}>
+			<GlobalContainer>
+				<div className={classes.containerTypesCards}>
+					<div className={classes.morphBlock}>
+						<img
+							className={classes.mihoMainImage}
+							src={isDesktop ? MihoMorph : MihoMorphMobile}
+							alt='miho'
+						/>
 
-          <div className={classes.contentBlock}>
-            <h4 className={classes.classiqueParagraph}>
-              <img
-                className={classes.classiqueBorder}
-                src={first}
-                alt="classique"
-              />
-              <p className={classes.title}>CLASSIQUE</p>
-            </h4>
-            <Paragraph className={`${classes.descr} ${classes.mihoText}`}>
-              {isDesktop ? morphIDText.classiqueDesktop: morphIDText.classiqueMobile}
-            </Paragraph>
-            <img className={classes.idImage} src={MihoID} alt="miho id" />
-          </div>
-        </div>
+						<div className={classes.contentBlock}>
+							<h4 className={classes.classiqueParagraph}>
+								<img
+									className={classes.classiqueBorder}
+									src={first}
+									alt='classique'
+								/>
+								<p className={classes.title}>CLASSIQUE</p>
+							</h4>
+							<Paragraph className={`${classes.descr} ${classes.mihoText}`}>
+								{isDesktop
+									? morphIDText.classiqueDesktop
+									: morphIDText.classiqueMobile}
+							</Paragraph>
+							<img className={classes.idImage} src={MihoID} alt='miho id' />
+						</div>
+					</div>
 
-        <div className={`${classes.morphBlock} ${classes.eternalBlock}`}>
-          <div className={classes.contentBlock}>
-            <h4 className={classes.eternalParagraph}>
-              <img
-                className={classes.eternalBorder}
-                src={second}
-                alt="eternal border"
-              />
-              <p className={`${classes.title} ${classes.eternalTitle}`}>
-                ETERNAL
-              </p>
-            </h4>
-            <Paragraph className={`${classes.descr} ${classes.henriText}`}>
-              {isDesktop ? morphIDText.eternalDesktop: morphIDText.eternalMobile}
-            </Paragraph>
-            <img
-              className={`${classes.idImage} ${classes.idImageHenri}`}
-              src={HenriID}
-              alt="hneri id card"
-            />
-          </div>
+					<div className={`${classes.morphBlock} ${classes.eternalBlock}`}>
+						<div className={classes.contentBlock}>
+							<h4 className={classes.eternalParagraph}>
+								<img
+									className={classes.eternalBorder}
+									src={second}
+									alt='eternal border'
+								/>
+								<p className={`${classes.title} ${classes.eternalTitle}`}>
+									ETERNAL
+								</p>
+							</h4>
+							<Paragraph className={`${classes.descr} ${classes.henriText}`}>
+								{isDesktop
+									? morphIDText.eternalDesktop
+									: morphIDText.eternalMobile}
+							</Paragraph>
+							<img
+								className={`${classes.idImage} ${classes.idImageHenri}`}
+								src={HenriID}
+								alt='hneri id card'
+							/>
+						</div>
 
-          <img className={classes.henriMainImage} src={isDesktop ? HenriMorph: HenriMorphDesktop} alt="henri morph" />
-        </div>
-      </div>
-    </GlobalContainer>
-    </div>
-  );
+						<img
+							className={classes.henriMainImage}
+							src={isDesktop ? HenriMorph : HenriMorphDesktop}
+							alt='henri morph'
+						/>
+					</div>
+				</div>
+			</GlobalContainer>
+		</div>
+	);
 }
