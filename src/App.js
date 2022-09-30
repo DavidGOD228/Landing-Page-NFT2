fe535349bc51f55be30fe3adfd5a1370a3dd55bb
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-09-30 19:12:03 +0300

    fix loader

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ function App() {
 
   const [isLoading, setIsloading] = useState(true);
 
+  return <Loader setIsloading={setIsloading} />
+
   useEffect(() => {
     window.onbeforeunload = function () {
       window.scrollTo(0, 0);
