3d5d0a7e49b4202f0115947db369cdbb53a4f0a4
Anastasia Karachova <nastiakarachova@gmail.com>
2022-09-30 11:49:14 +0300

    fix order at first live on mobile

--- src/components/common/GlobalContainer/GlobalContainer.js
+++ src/components/common/GlobalContainer/GlobalContainer.js
@@ -1,8 +1,8 @@
 import classes from "./style.module.scss";
 
-export function GlobalContainer({children, className, ...props}) {
+export function GlobalContainer({children, className, style, ...props}) {
   return (
-    <div className={`${classes.globalContainer} ${className}`} {...props}>
+    <div className={`${classes.globalContainer} ${className}`} style={style} {...props}>
       {children}
     </div>
   );
