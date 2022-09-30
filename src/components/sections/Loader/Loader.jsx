dea3ee8c8551d1e781e3e47966121b47b64a817d
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-09-30 16:29:09 +0300

    anim loader

--- src/components/sections/Loader/Loader.jsx
+++ src/components/sections/Loader/Loader.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
 import loaderTitle from 'assets/images/loader-title.png';
-import CountUp from 'react-countup';
 import classes from './style.module.scss';
 import {motion} from 'framer-motion'
 
+const maxCount = 100;
+const duration = 5; // sec
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 export function Loader({ setIsloading }) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+		if (count >= 100) setIsloading(false);
+
+		if (count < 100) {
+			setTimeout(() => {
+				if (maxCount - count <= maxCount / duration) return setCount(100);
+
+				setCount(s => {
+					const addNumLoadCount = randomIntFromInterval(1, maxCount / duration);
+					return s + addNumLoadCount;
+				});
+			}, Math.random() * 3000);
+		}
+	}, [count, setIsloading]);
+
   return (
       <motion.div
         className={classes.loaderWrapper}
@@ -18,14 +42,16 @@ export function Loader({ setIsloading }) {
           <div className={classes.shadow} />
           <img className={classes.loaderTitleImg} src={loaderTitle} alt="" />
         </div>
-        <CountUp
+        <div
           className={classes.counterLoader}
           duration={5}
           start={0}
           end={100}
           suffix={'%'}
           onEnd={() => setIsloading(false)}
-        />
+        >
+          {count}%
+        </div>
       </motion.div>
   )
 }
