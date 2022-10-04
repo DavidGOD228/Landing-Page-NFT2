20a8cf3f2b08149a5720f2f4c2b9dfd103d4ac85
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-10-04 12:00:23 +0300

    texts roadmap br

--- src/components/sections/Roadmap/data/running.js
+++ src/components/sections/Roadmap/data/running.js
@@ -6,10 +6,12 @@ export const roadmapTableData = [
     headerBackgroundColor: 'linear-gradient(90deg, rgba(253, 204, 143, 1) 0%, rgba(31, 249, 236, 1) 100%)',
     borderColor: '#fccb8f',
     subtitle: <p>
-      Market Research. <br/>
-      Creating graphic language, art concept, 3D content. <br/>
-      Technology verification. <br/>
-      Project concept development. <br/>
+      Market Research.<br/>
+      Creating graphic language<br/>
+      Art concept<br/>
+      3D content.<br/>
+      Technology verification.<br/>
+      Project concept development.<br/>
       Game engine R&D.
     </p>
   },
@@ -20,9 +22,9 @@ export const roadmapTableData = [
     headerBackgroundColor: 'linear-gradient(90deg, rgba(36, 249, 232, 1) 0%, rgba(199, 247, 121, 1) 100%)',
     borderColor: '#23f8ea',
     subtitle: <p>
-      Founders funding round. Preparing development roadmap.
-      Team recruitment to implement AAA-level game.
-      Start of game development.
+      Founders funding round. Preparing development roadmap.<br/>
+      Team recruitment to implement AAA-level game.<br/>
+      Start of game development.<br/>
       Define decentralized parts of the game.
     </p>
   },
@@ -33,11 +35,11 @@ export const roadmapTableData = [
     headerBackgroundColor: 'linear-gradient(90deg, rgba(201, 247, 120, 1) 0%, rgba(254, 217, 138, 1) 100%)',
     borderColor: '#c9f778',
     subtitle: <p>
-      Project Team Scale.
-      Game development on-going.
-      Morphs assets.
-      Basic environment assets.
-      Morphs outfits assets.
+      Project Team Scale.<br/>
+      Game development on-going.<br/>
+      Morphs assets.<br/>
+      Basic environment assets.<br/>
+      Morphs outfits assets.<br/>
       Project Lite paper.
     </p>
   },
@@ -47,13 +49,13 @@ export const roadmapTableData = [
     title: 'Official Announcements & Seed Round',
     borderColor: '#fdda89',
     subtitle: <p>
-      In-game economy part implemented
-      Game Graphics
-      NFT Teaser Realize
-      Community building
-      Website v1 Launch
-      Seed round
-      AMA & Events
+      In-game economy part implemented<br/>
+      Game Graphics<br/>
+      NFT Teaser Realize<br/>
+      Community building<br/>
+      Website v1 Launch<br/>
+      Seed round<br/>
+      AMA & Events<br/>
       Ambassador Program
     </p>
   },
@@ -63,11 +65,11 @@ export const roadmapTableData = [
     title: 'Internal test of beta version & Strategic round',
     borderColor: '#5A5A6B',
     subtitle: <p>
-      PR Marketing Campaign
-      KOLs onboarding
-      Strategic round
-      Internal tests of beta game realize
-      WEB 3.0 Penetration Testing Official Game Trailer
+      PR Marketing Campaign<br/>
+      KOLs onboarding<br/>
+      Strategic round<br/>
+      Internal tests of beta game realize<br/>
+      WEB 3.0 Penetration Testing Official Game Trailer<br/>
       Bug Bounty Program Website v2 Realize
     </p>
   },
