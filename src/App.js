283a987c3d55fb35becacc83b275468fd06a5a2d
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-10-05 16:17:20 +0300

    test setions

--- src/App.js
+++ src/App.js
@@ -1,68 +1,12 @@
-import { useEffect, useState } from 'react';
 import {
-	TopBlock,
-	FataMorgana,
 	Footer,
-	FirstLiveSimulator,
-	LikeHuman,
-	Freedom,
-	Earn,
-	YourMorph,
 	Personalize,
-	MorphId,
-	MorphsUnique,
-	Wizard,
-	Foto,
-	CombinedGame,
-	Roadmap,
-	TypesCards,
-	Privacy,
-  Loader
 } from 'components';
-import { useWindowWidth } from 'hooks/useWindowWidth';
-import { media } from 'utils/media';
 
 function App() {
-	const isDesktop = useWindowWidth(media.desktop);
-	const isTablet = useWindowWidth(media.tablet);
-
-  const [isLoading, setIsloading] = useState(true);
-  
-
-  useEffect(() => {
-    window.onbeforeunload = function () {
-      window.scrollTo(0, 0);
-    }
-    document.body.style.overflowY = 'hidden';
-    document.body.style.height = '100vh';
-    document.body.style.pointerEvents = 'none';
-
-    if(!isLoading) {
-      document.body.style.pointerEvents = 'auto';
-      document.body.style.overflowY = 'auto';
-      document.body.style.height = 'auto';
-    }
-  }, [isLoading]);
-
 	return (
 		<>
-			{isLoading && <Loader setIsloading={setIsloading} />}
-      <TopBlock />
-      <FirstLiveSimulator />
-			<LikeHuman />
-			{!isDesktop && <Privacy />}
-			<FataMorgana />
-			<Freedom />
 			<Personalize />
-			<Earn />
-      <MorphId />
-			<TypesCards />
-			<YourMorph />
-			<MorphsUnique />
-			<Roadmap />
-			<Wizard />
-			{!isTablet && <Foto />}
-			<CombinedGame />
 			<Footer />
 		</>
 	);
