6b00d810d56f1204355d3d91d620da3785b01f0d
Anastasia Karachova <nastiakarachova@gmail.com>
2022-10-12 10:31:42 +0300

    feat: add background for header at four column

--- src/components/sections/Roadmap/components/Column/Column.jsx
+++ src/components/sections/Roadmap/components/Column/Column.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import classes from './style.module.scss'
 
 const Column = ({data}) => {
-  const {headerText, year, title, subtitle, borderColor, headerBackgroundColor} = data
+  const {headerText, year, title, subtitle, borderColor, headerBackgroundColor, headerColor} = data
   return (
     <div className={classes.column} style={{ borderLeft: `1px solid ${borderColor}`}}>
       <div style={{background: headerBackgroundColor}} className={classes.header}>
         <h3 className={classes.headerText}>{headerText}</h3>
-        <span style={{color: headerBackgroundColor && '#0B0B0C'}} className={classes.year}>{year}</span>
+        <span style={{color: headerColor ? headerColor : '#fff'}} className={classes.year}>{year}</span>
       </div>
       <div className={classes.content}>
         <h4 className={classes.title}>{title}</h4>
