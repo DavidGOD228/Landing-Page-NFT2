de3c5f802a39b1b722c903d76fb8758647023c76
Anastasia Karachova <nastiakarachova@gmail.com>
2022-10-27 12:52:52 +0300

    fix: replace images on marketplace

--- src/components/sections/Personalize/Items/Item/Item.jsx
+++ src/components/sections/Personalize/Items/Item/Item.jsx
@@ -1,22 +1,19 @@
 import classes from './style.module.scss';
 import {getImageSrc} from "utils/createMarketplaceLink";
-import {isWebpSupported} from 'react-image-webp/dist/utils';
 
 export default function Item({ firstImage, secondImage, isFlipped }) {
-  const isWebpFirstImage = isWebpSupported ? firstImage.replace('.png', ".webp") : firstImage;
-  const isWebpSecondImage = isWebpSupported ? secondImage.replace('.png', ".webp") : firstImage;
 
 	return (
 		<div className={classes.scene}>
 			<div className={`${classes.card} ${isFlipped && classes.isFlipped}`}>
         <img
           className={classes.cardFace}
-          src={getImageSrc(isWebpFirstImage)}
+          src={getImageSrc(firstImage)}
           alt='imgName'
         />
         <img
           className={`${classes.cardFace} ${classes.cardFaceBack}`}
-          src={getImageSrc(isWebpSecondImage)}
+          src={getImageSrc(secondImage)}
           alt='imgName'
         />
 			</div>
